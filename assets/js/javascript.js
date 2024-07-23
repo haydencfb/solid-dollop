0313ddedb16d1bed0134cfa3a5f19a8ff3e871c2
unknown <you@example.com>
2024-07-22 20:21:13 -0700

    Home page player stat modal

---
+++ assets/js/javascript.js
@@ -0,0 +1,20 @@
+// Home Screen - Modal
+var modal=document.getElementById("player-stats");
+
+var button = document.getElementById("player-stats-modal");
+
+var span = document.getElementsByClassName("close")[0];
+
+button.onclick = function(){
+    modal.style.display= "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
