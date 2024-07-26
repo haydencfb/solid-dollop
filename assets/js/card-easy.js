80fc23cbbb055e2335513088ff94e06d7d78ca7e
unknown <you@example.com>
2024-07-25 21:00:29 -0700

    push

--- assets/js/card-easy.js
+++ assets/js/card-easy.js
@@ -3,8 +3,29 @@ document.addEventListener('DOMContentLoaded', (event) => {
     var cards = document.querySelectorAll('.card');
 
     cards.forEach((card) => {
-        card.addEventListener('click', function() {
+        card.addEventListener('click', function () {
             card.classList.toggle('flipped');
         });
     });
 });
+
+var minLabel = document.getElementById("min");
+var secLabel = document.getElementById("sec");
+var totalSec = 0;
+setInterval(setTime, 1000);
+
+function setTime() {
+    ++totalSec;
+    secLabel.innerHTML = pad(totalSec % 60);
+    minLabel.innerHTML = pad(parseInt(totalSec / 60));
+}
+
+function pad(val) {
+    var valString = val + "";
+    if (valString.length < 2) {
+        return "0" + valString;
+    }
+    else {
+        return valString;
+    }
+}
