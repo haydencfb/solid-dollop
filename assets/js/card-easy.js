b5a7f6d09cec7e93e7c0d7a9ab5c37ae2865341b
Isaiah Skidmore <isaiahschardt0617@gmail.com>
2024-07-25 19:22:59 -0700

    added event listeners for all the card pages

---
+++ assets/js/card-easy.js
@@ -0,0 +1,10 @@
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    var cards = document.querySelectorAll('.card');
+
+    cards.forEach((card) => {
+        card.addEventListener('click', function() {
+            card.classList.toggle('flipped');
+        });
+    });
+});
