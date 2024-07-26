91d8065f8be34e6488581cc9d5ad92d86adfec6a
unknown <you@example.com>
2024-07-25 21:52:09 -0700

    timer logic

--- assets/js/card-easy.js
+++ assets/js/card-easy.js
@@ -67,3 +67,24 @@ Array.from(cardFronts).forEach((cardFront, index) => {
         }
     }
 });
+
+var minLabel = document.getElementById("min");
+var secLabel = document.getElementById("sec");
+var totalSec = 0;
+setInterval(setTime, 1000);
+
+function setTime() {
+    ++totalSec;
+    secLabel.innerHTML = pad(totalSec % 60);
+    minLabel.innerHTML = pad(parseInt(totalSec / 60));
+}
+
+function pad(val) {
+    var valString = val + "";
+    if (valString.length < 2) {
+        return "0" + valString;
+    }
+    else {
+        return valString;
+    }
+}
--- assets/js/card-medium.js
+++ assets/js/card-medium.js
@@ -72,3 +72,24 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
     }
 });
+
+var minLabel = document.getElementById("min");
+var secLabel = document.getElementById("sec");
+var totalSec = 0;
+setInterval(setTime, 1000);
+
+function setTime() {
+    ++totalSec;
+    secLabel.innerHTML = pad(totalSec % 60);
+    minLabel.innerHTML = pad(parseInt(totalSec / 60));
+}
+
+function pad(val) {
+    var valString = val + "";
+    if (valString.length < 2) {
+        return "0" + valString;
+    }
+    else {
+        return valString;
+    }
+}
